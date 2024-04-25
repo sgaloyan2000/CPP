5c536bc4fb9ac5b89566b7ce145ef49fd4c8c9d9
sgaloyan2000 <99962406+sgaloyan2000@users.noreply.github.com>
2024-04-25 15:52:06 +0400

    Update class_car.cpp
    Inheritence

--- class_car.cpp
+++ class_car.cpp
@@ -1,165 +1,235 @@
 #include <iostream>
 #include <string>
-class wheel{
-  double m_diameter;
-  bool m_winter; //true if winter, false if summer
-public:
-    wheel(){
-        m_diameter = 16;
-        m_winter = true;
-    }
-    wheel(double diameter, bool winter):m_diameter(diameter),m_winter(winter){
-        
-    }
-    void info(){
-        std::cout<<"Diameter of wheels are - "<<m_diameter<<std::endl;
-        if (m_winter)
-            std::cout<<"You can drive if outside is winter"<<std::endl;
-        else
-            std::cout<<"You can drive if outside is summer"<<std::endl;
-    }
-    void change_diameter(double new_diameter){
-        if(new_diameter>14 && new_diameter <22)
-            m_diameter = new_diameter;  
-        else
-            std::cout<<"There is no such Diameter"<<std::endl;
-    }
-    void switch_wheels(){
-        m_winter = !m_winter;
-    }
-    
-};
-class car{
+
+class vehicle{
     int year;
-    std::string mark;
-    double engine;
-    std::string color;
-    wheel wheels;
     
 public:
-    car(){}
-    car(int input_year, std::string input_mark, double input_engine, std::string input_color){
-        if(1990<=input_year && input_year<=2024)
-            year = input_year;
-        else std::cout<<"Input data is not valid";
-        if(input_mark == "Mercedes" || input_mark == "BMW")
-            mark = input_mark;
-        else std::cout<<"Input mark is not valid";
-        if(input_engine<= 6 && input_engine >=1)
-            engine = input_engine;
-        else std::cout<<"Input engine power is not valid";
-        if(input_color == "green" || input_color == "blue" || input_color == "red" ||
-        input_color == "black" || input_color == "white")
-            color = input_color;
-        else std::cout<<"Input color is not valid";
+    vehicle()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    void get_info(){
-        std::cout<<"Dear user, you are driving "<<color<<" "<<mark<<" which is made in "<<year<<" ,its engine power is "
-        <<engine<<", wheels: ";
-        wheels.info();
+    
+    vehicle(int input_year):year(input_year)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
     }
-    void change_diameter(double new_diameter){
-        wheels.change_diameter(new_diameter);
+    ~vehicle()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    void switch_wheels(){
-        wheels.switch_wheels();
+
+};
+
+class car:public vehicle{
+    std::string mark;
+public:
+    car()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    car(int input_year, std::string input_mark):vehicle(input_year),mark(input_mark)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~car()
+    {
+        std::cout<<__func__<<std::endl;
     }
 };
-class parking{
-  car** cars;
-  int size;
-  int ocupied;
- public:
-    parking(){
-        size = 0;
-        ocupied = 0;
-        cars = nullptr;
+class bus:public vehicle{
+    int seats;
+public:
+    bus()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    parking(int size_){
-        cars = new car*[size_];
-        size = size_;
-        ocupied = 0;
+    bus(int input_year, int input_seats):vehicle(input_year),seats(input_seats)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
     }
-    parking (const parking& obj){
-        size = obj.size;
-        ocupied = obj.ocupied;
-        cars = new car*[size];
-        for (int i=0; i<size; i++){
-            cars[i] = new car(*obj.cars[i]);
-        }
+    ~bus()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    parking& operator=(const parking& obj) {
-        if (this != &obj) {
-    
-            delete[] cars;
+};
+class tank:public vehicle{
+    int ammo;
+public:
+    tank()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    tank(int input_year,int input_ammo):vehicle(input_year),ammo(input_ammo)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~tank()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
+class sedan:public car{
+    bool gas;
+public:
+    sedan()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    sedan(int input_year, std::string input_mark, bool is_gas):car(input_year,input_mark),gas(is_gas)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~sedan()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
+class jeep:public car{
+    double wheels_diameter;
+public:
+    jeep()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    jeep(int input_year,std::string input_mark, double input_diameter):
+    car(input_year,input_mark),wheels_diameter(input_diameter)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~jeep()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
+class electro:public car{
+    bool automatic;
+public:
+    electro()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    electro(int input_year,std::string input_mark, bool input_automatic):car(input_year,input_mark),
+    automatic(input_automatic)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~electro()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
+class two_stage:public bus{
+    std::string color;
+public:
+    two_stage()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    two_stage(int input_year,int input_seats,std::string input_color):
+    bus(input_year,input_seats),color(input_color)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~two_stage()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
+class micro:public bus{
+    bool conditioner;
+public:
+    micro()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    micro(int input_year,int input_seats,bool input_conditioner):bus(input_year,input_seats),
+    conditioner(input_conditioner)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~micro()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
+class tour:public bus{
+    std::string direction;
+public:
+    tour()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    tour(int input_year,int input_seats,std::string input_direction):bus(input_year,input_seats),
+    direction(input_direction)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~tour()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
 
-            size = obj.size;
-            ocupied = obj.ocupied;
-    
-            cars = new car*[size];
-            for (int i = 0; i < size; ++i) {
-                cars[i] = new car(*obj.cars[i]);
-            }
-        }
-        return *this;
+class armada: public tank{
+    bool rocket;
+public:
+    armada()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    ~parking(){
-        // for (int i = 0; i < size; i++)
-        //     delete cars[i];
-        delete[] cars;
+    armada(int input_year,int input_ammo,bool input_rocket):tank(input_year,input_ammo),rocket(input_rocket)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
     }
+    ~armada()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+};
 
-    void push(car* new_car){
-        if(ocupied < size){
-            cars[ocupied] = new_car;
-            ocupied++;
-            std::cout << "Car added successfully." << std::endl;
-            std::cout << ocupied<<" from "<<size<<" slots are ocupied" << std::endl;
-        }
-        else std::cout<<"There is no free parking slot"<<std::endl;
+class panzer: public tank{
+    bool gun;
+public:
+    panzer()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    void pop(){
-        if(ocupied > 0){
-            std::cout << "Removing car from parking slot " << ocupied - 1 << std::endl;
-            //delete cars[ocupied-1]; 
-            cars[ocupied-1] = nullptr;
-            ocupied--;
-            std::cout << "Car removed successfully." << std::endl;
-        }
-        else std::cout << "Parking slot is empty" << std::endl;
+    panzer(int input_year,int input_ammo,bool input_gun):tank(input_year,input_ammo),gun(input_gun)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
     }
-
-    void parking_info(){
-        if(ocupied){
-            for (int i=0;i<size;i++)
-                cars[i]->get_info();
-        }
-        else std::cout<<"There is no cars in parking"<<std::endl;
+    ~panzer()
+    {
+        std::cout<<__func__<<std::endl;
     }
-    
+};
+class T30: public tank{
+    bool armor;
+public:
+    T30()
+    {
+        std::cout<<__func__<<std::endl;
+    }
+    T30(int input_year,int input_ammo,bool input_armor):tank(input_year,input_ammo),armor(input_armor)
+    {
+        std::cout<<__func__<<" parametrized"<<std::endl;
+    }
+    ~T30()
+    {
+        std::cout<<__func__<<std::endl;
+    }
 };
 int main() {
-    car my_car1(2020,"Mercedes",4,"red");
-    car* car_1 = &my_car1;
-    car my_car2(2002,"Mercedes",3,"green");
-    car* car_2 = &my_car2;
-    car my_car3(2019,"BMW",2,"black");
-    car* car_3 = &my_car3;
-    car my_car4(2012,"Mercedes",1.5,"white");
-    car* car_4 = &my_car4;
-    car my_car5(2022,"BMW",5,"red");
-    car* car_5 = &my_car5;
-    car my_car6(2007,"BMW",2,"white");
-    car* car_6 = &my_car6;
-    parking park(5);
-    park.push(car_1);
-    park.push(car_2);
-    park.push(car_3);
-    park.push(car_4);
-    park.push(car_5);
-    park.push(car_6);
-    park.pop();
-    park.push(car_6);
-    park.parking_info();
+    sedan bmw;
+    jeep range_rover;
+    electro tesla;
+    two_stage bus_2stage;
+    micro micro_bus;
+    tour tour_bus;
+    armada tank_armada;
+    panzer tank_panzer;
+    T30 tank_T30;
+
     return 0;
 }
