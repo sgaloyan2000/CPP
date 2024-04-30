24f5098991a7ec3998817c9fa6b4fef8b84758b5
sgaloyan2000 <99962406+sgaloyan2000@users.noreply.github.com>
2024-04-30 15:09:24 +0400

    Update animals.cpp
    Making abstract class animal with pure virtual function

--- animals.cpp
+++ animals.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 class animal{
 public:
-    virtual void voice(){
-        std::cout<<"animal voice"<<std::endl;
-    }
+    animal (){ std:: cout << __func__<< std::endl;}
+    virtual ~animal (){ std:: cout << __func__<< std::endl;}
+    virtual void voice() = 0;
 };
 class dog:public animal{
 public:
+    dog(){ std:: cout << __func__<< std::endl;}
+    ~dog(){ std:: cout << __func__<< std::endl;}
     virtual void voice(){
         std::cout<<"shun - hachoc"<<std::endl;
     } 
 };
 class cat:public animal{
 public:
+    cat(){ std:: cout << __func__<< std::endl;}
+    virtual ~cat(){ std:: cout << __func__<< std::endl;}
     virtual void voice(){
         std::cout<<"katu - mlavoc"<<std::endl;
     } 
 };
 class fish:public animal{
 public:
+    fish(){ std:: cout << __func__<< std::endl;}
+    virtual ~fish(){ std:: cout << __func__<< std::endl;}
     virtual void voice(){
         std::cout<<"dzuk - yes dzayn chem hanum"<<std::endl;
     } 
 };
 class lion:public animal{
 public:
+    lion(){ std:: cout << __func__<< std::endl;}
+    virtual ~lion(){ std:: cout << __func__<< std::endl;}
     virtual void voice(){
         std::cout<<"aryuc - mrnchoc"<<std::endl;
     } 
 };
 class wolf:public animal{
 public:
+    wolf(){ std:: cout << __func__<< std::endl;}
+    virtual ~wolf(){ std:: cout << __func__<< std::endl;}
     virtual void voice(){
         std::cout<<"gayl - vornoc"<<std::endl;
     } 
