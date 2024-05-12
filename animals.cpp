28612dcd420069d759f1bad7989301a1e82e4970
sgaloyan2000 <99962406+sgaloyan2000@users.noreply.github.com>
2024-05-12 17:24:33 +0400

    Update animals.cpp
    Added singletone class zoo

--- animals.cpp
+++ animals.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+
 class animal{
     static int count;
 public:
-    animal (){ 
-        std:: cout << __func__<< std::endl;
+    animal (){
         count++;
     }
-    virtual ~animal (){ std:: cout << __func__<< std::endl;}
-    int get_count(){
+    virtual ~animal (){}
+    static int get_count(){
         return count;
     }
     virtual void voice() = 0;
@@ -17,61 +17,103 @@ int animal::count = 0;
 
 class dog:public animal{
 public:
-    dog(){ std:: cout << __func__<< std::endl;}
-    ~dog(){ std:: cout << __func__<< std::endl;}
+    dog(){}
+    ~dog(){}
     virtual void voice(){
         std::cout<<"shun - hachoc"<<std::endl;
-    } 
+    }
 };
 class cat:public animal{
 public:
-    cat(){ std:: cout << __func__<< std::endl;}
-    virtual ~cat(){ std:: cout << __func__<< std::endl;}
+    cat(){}
+    virtual ~cat(){}
     virtual void voice(){
         std::cout<<"katu - mlavoc"<<std::endl;
-    } 
+    }
 };
 class fish:public animal{
 public:
-    fish(){ std:: cout << __func__<< std::endl;}
-    virtual ~fish(){ std:: cout << __func__<< std::endl;}
+    fish(){}
+    virtual ~fish(){}
     virtual void voice(){
         std::cout<<"dzuk - yes dzayn chem hanum"<<std::endl;
-    } 
+    }
 };
 class lion:public animal{
 public:
-    lion(){ std:: cout << __func__<< std::endl;}
-    virtual ~lion(){ std:: cout << __func__<< std::endl;}
+    lion(){}
+    virtual ~lion(){}
     virtual void voice(){
         std::cout<<"aryuc - mrnchoc"<<std::endl;
-    } 
+    }
 };
 class wolf:public animal{
 public:
-    wolf(){ std:: cout << __func__<< std::endl;}
-    virtual ~wolf(){ std:: cout << __func__<< std::endl;}
+    wolf(){}
+    virtual ~wolf(){}
     virtual void voice(){
         std::cout<<"gayl - vornoc"<<std::endl;
-    } 
+    }
 };
-int main()
-{   
+
+class zoo{
+    static zoo* instance;
     animal* ptr[5];
-    ptr[0] = new dog();
-    ptr[1] = new cat();
-    ptr[2] = new fish();
-    ptr[3] = new lion();
-    ptr[4] = new wolf();
-    
-    std::cout<<ptr[0]->get_count()<<std::endl;
-    
-    for(int i = 0; i < 5; ++i) {
-        ptr[i]->voice();
+    zoo(){}
+public:
+    zoo(const zoo& obj) = delete;
+    zoo& operator = (const zoo& obj) = delete;
+    static zoo* get_instance()
+    {
+        if(instance)
+            return instance;
+        else
+        {
+            instance = new zoo();
+            return instance;
+        }
     }
-    
-    for(int i = 0; i < 5; ++i) {
-        delete ptr[i];
+    void registration(dog* d, cat* c, fish* f, lion* l, wolf* w){
+        ptr[0] = d;
+        ptr[1] = c;
+        ptr[2] = f;
+        ptr[3] = l;
+        ptr[4] = w;
     }
+    void zoo_voices(){
+        for(int i = 0; i < 5; ++i) {
+            ptr[i]->voice();
+        }
+    }
+    ~zoo()
+    {
+        delete instance;
+    }
+};
+
+zoo* zoo::instance = nullptr;
+
+int main()
+{
+    zoo* newzoo = zoo::get_instance();
+    
+    dog* d = new dog();
+    cat* c = new cat();
+    fish* f = new fish();
+    lion* l = new lion();
+    wolf* w = new wolf();
+    
+    newzoo->registration(d,c,f,l,w);
+    newzoo->zoo_voices();
+    
+    std::cout<<animal::get_count()<<std::endl;
+    
+    
+    delete d;
+    delete c;
+    delete f;
+    delete l;
+    delete w;
+    
     return 0;
 }
