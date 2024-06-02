a36991d25f324895ebe4d23fbde15d046fc97f26
sgaloyan2000 <99962406+sgaloyan2000@users.noreply.github.com>
2024-06-02 16:22:05 +0400

    Update main.cpp

--- MySTL/forward_list/main.cpp
+++ MySTL/forward_list/main.cpp
@@ -2,12 +2,11 @@
 #include "forward_list.hpp"
 int main() {
     forward_list<int> fl;
+    std::cout<<fl.empty()<<std::endl;
     fl.push_front(3);
     fl.push_front(33);
     fl.push_front(123);
-    fl.push_front(43);
-    fl.push_front(32);
-    fl.push_front(31);
+    fl.push_back(123412351);
     fl.display();
     std::cout<<std::endl;
     fl.pop_front();
@@ -15,8 +14,10 @@ int main() {
     fl.insert(4022, 2);
     fl.display();
     std::cout<<std::endl;
-    std::cout<<fl.empty();
-    std::cout<<std::endl;
+    std::cout<<fl.has_cycle()<<std::endl;
+    fl.make_cycle();
+//    fl.display();
+    std::cout<<fl.has_cycle()<<std::endl;
     
     return 0;
 }
