aa9a2b5b33f986785a8246669f07275e836a2192
sgaloyan2000 <99962406+sgaloyan2000@users.noreply.github.com>
2024-06-04 01:59:44 +0400

    Update main.cpp

--- MySTL/forward_list/main.cpp
+++ MySTL/forward_list/main.cpp
@@ -2,22 +2,25 @@
 #include "forward_list.hpp"
 int main() {
     forward_list<int> fl;
-    std::cout<<fl.empty()<<std::endl;
+    //std::cout<<fl.empty()<<std::endl;
     fl.push_front(3);
-    fl.push_front(33);
-    fl.push_front(123);
-    fl.push_back(123412351);
+    fl.push_front(2);
+    fl.push_front(1);
+    fl.push_back(4);
+    //fl.display();
+    //std::cout<<std::endl;
+    //fl.pop_front();
+    //fl.pop_front();
+    fl.insert(5, 4);
     fl.display();
     std::cout<<std::endl;
-    fl.pop_front();
-    fl.pop_front();
-    fl.insert(4022, 2);
-    fl.display();
+    //std::cout<<fl.has_cycle()<<std::endl;
+    //fl.make_cycle();
+    //fl.display();
+    //std::cout<<fl.has_cycle()<<std::endl;
+    //fl.reverse();
+    std::cout<<fl.get_n_element_from_back(1)<<std::endl;
     std::cout<<std::endl;
-    std::cout<<fl.has_cycle()<<std::endl;
-    fl.make_cycle();
-//    fl.display();
-    std::cout<<fl.has_cycle()<<std::endl;
-    
+    fl.display();
     return 0;
 }
