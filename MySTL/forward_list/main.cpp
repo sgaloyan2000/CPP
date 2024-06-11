365cb5c5c524345590e18b212e77bee93fba6319
sgaloyan2000 <99962406+sgaloyan2000@users.noreply.github.com>
2024-06-11 14:42:57 +0400

    Update main.cpp

--- MySTL/forward_list/main.cpp
+++ MySTL/forward_list/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include "forward_list.hpp"
+template <class T>
+void print(forward_list<T> fl, typename forward_list<T>::input_iterator beg, typename forward_list<T>::input_iterator end){
+    while (beg != end) {
+        std::cout<<*beg<<" ";
+        beg++;
+    }
+}
 int main() {
     forward_list<int> fl;
     //std::cout<<fl.empty()<<std::endl;
@@ -12,18 +19,18 @@ int main() {
     //fl.pop_front();
     //fl.pop_front();
     fl.insert(5, 4);
-    fl.display();
+//    fl.display();
     std::cout<<std::endl;
     //std::cout<<fl.has_cycle()<<std::endl;
     //fl.make_cycle();
     //fl.display();
     //std::cout<<fl.has_cycle()<<std::endl;
     //fl.reverse();
-    std::cout<<fl.get_n_element_from_back(0)<<std::endl;
-    std::cout<<std::endl;
-    fl.display();
+//    std::cout<<fl.get_n_element_from_back(0)<<std::endl;
+//    std::cout<<std::endl;
+//    fl.display();
 //    fl.reverse();
 //    fl.display();
-    
+    print(fl, fl.begin(),fl.end());
     return 0;
 }
